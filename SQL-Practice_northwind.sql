27d1c34a7038a5a4ce6b2aec5a08e4e9bb3c0d4d
Dylan Whitener <91494455+DylanWhitener@users.noreply.github.com>
2023-07-18 12:25:40 -0700

    Update SQL-Practice_northwind.sql
    Medium and hard queries added

--- SQL-Practice_northwind.sql
+++ SQL-Practice_northwind.sql
@@ -76,3 +76,60 @@ select
     sum(units_in_stock) as total_units_in_stock,
     sum(discontinued) as total_discontinued
 from products
+
+-- Medium Question difficulty
+-- 1: Show the ProductName, CompanyName, CategoryName from the products, suppliers, and categories table
+select
+	prod.product_name,
+    cat.category_name,
+    sup.company_name
+from products prod
+join categories cat on prod.category_id = cat.category_id
+join suppliers sup on prod.supplier_id = sup.supplier_id
+
+-- 2: Show the category_name and the average product unit price for each category rounded to 2 decimal places.
+select
+	cat.category_name,
+    round(avg(prod.unit_price),2) as avg_product_unit_price
+from categories cat
+join products prod ON cat.category_id = prod.category_id
+group by category_name
+
+-- 3: Show the city, company_name, contact_name from the customers and suppliers table merged together.
+-- Create a column which contains 'customers' or 'suppliers' depending on the table it came from.
+select
+	city,
+    company_name,
+    contact_name,
+    'customers' as description
+from customers
+union
+select
+	city,
+    company_name,
+    contact_name,
+    'suppliers'
+from suppliers
+
+-- Hard Question difficulty
+-- 1: Show the employee's first_name and last_name, a "num_orders" column with a count of the orders taken, 
+-- and a column called "Shipped" that displays "On Time" if the order shipped on time and "Late" if the order shipped late.
+
+-- Order by employee last_name, then by first_name, and then descending by number of orders.
+select
+	employees.first_name,
+    employees.last_name,
+    count(order_id) as num_orders,
+    (case when shipped_date < required_date Then 'On Time'
+     else 'Late'
+     END) as Shipped
+from employees
+join orders on employees.employee_id = orders.employee_id
+group by
+	employees.first_name,
+    employees.last_name,
+    Shipped
+order by
+	employees.last_name,
+    employees.first_name,
+    num_orders desc
